3e88dcd7a068d5fa251f0e65e4c860ab0c36395a
AleksandrHodcko <Hodckoq@google.com>
2020-03-27 20:56:46 +0300

    test commit

--- web/src/main/java/com/hodcko/locale.java
+++ web/src/main/java/com/hodcko/locale.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 
 @WebServlet("/locale")
 public class locale extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String locale = req.getParameter("locale");
